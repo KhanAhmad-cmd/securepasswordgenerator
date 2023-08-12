3a7f689db4788ec398b9f09a4213cc38c021bc3c
Ahmad Khan <83468415+KhanAhmad-cmd@users.noreply.github.com>
2023-08-12 10:49:23 -0400

    Update passwordgenerator.py

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,10 +1,18 @@
+#Import random library
 import random
+
+#Initilaze the characters to include in the password
 lowercase = "abcdefghjkmnpqrstuvwxyz"
 Uppercase = "ABCDEFGHJKMNPQRSTUVWXYZ"
 Numerical = "23456789"
 SpecialChar = "#*&@$"
 
+#Add the strings
 string = lowercase + Numerical + SpecialChar + Uppercase
+
+#Specify the length of the password to generate
 length = input("Enter the number of character: ")
+
+#The generator
 password = "".join(random.sample(string, int(length)))
 print("Your secure password is: " + password)
